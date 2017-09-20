18cf96bee7f83a40144ffda404d2267fd77a596d
Rémi <remi.hector80@gmail.com>
2017-09-20 08:55:26 +0200

    ajout save bdd

---
+++ web/save_database/suivi_sportif(19Sept).sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 20 Septembre 2017 à 06:53
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `suivi_sportif`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+CREATE TABLE `cours` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `reference` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fos_user`
+--
+
+CREATE TABLE `fos_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `fos_user`
+--
+
+INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`) VALUES
+(1, 'onnatarashi', 'onnatarashi', 'remi.hector80@gmail.com', 'remi.hector80@gmail.com', 1, 'M2RYSLzQJu4KfwrlGZqBbbwiDoyRL/N0dFTWYq2ZE1Q', 'Y06IDfyEsyxSWApM/zCy2lkr7z3IEoaAGZDcuH4cwzkStWndQwBmjjEqC9Ln4GefpFGgVVnKnvLnOJCsm0wGFw==', '2017-09-19 09:45:05', NULL, NULL, 'a:2:{i:0;s:10:"ROLE_ADMIN";i:1;s:16:"ROLE_SUPER_ADMIN";}');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jour`
+--
+
+CREATE TABLE `jour` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `jour`
+--
+
+INSERT INTO `jour` (`id`, `nom`) VALUES
+(7, 'Dimanche'),
+(4, 'Jeudi'),
+(1, 'Lundi'),
+(2, 'Mardi'),
+(3, 'Mercredi'),
+(6, 'Samedi'),
+(5, 'Vendredi');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nutrition`
+--
+
+CREATE TABLE `nutrition` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `illustration` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `nutrition`
+--
+
+INSERT INTO `nutrition` (`id`, `menu`, `illustration`) VALUES
+(1, 'Riz - Dinde', ''),
+(2, 'toto', '7bcb6f0f9ac6a3417f60a863d8c694a2.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `programme`
+--
+
+CREATE TABLE `programme` (
+  `id` int(11) NOT NULL,
+  `nutrition_id` int(11) DEFAULT NULL,
+  `jour_id` int(11) DEFAULT NULL,
+  `cours_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_FDCA8C9C6C6E55B5` (`nom`);
+
+--
+-- Index pour la table `fos_user`
+--
+ALTER TABLE `fos_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
+  ADD UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`),
+  ADD UNIQUE KEY `UNIQ_957A6479C05FB297` (`confirmation_token`);
+
+--
+-- Index pour la table `jour`
+--
+ALTER TABLE `jour`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_DA17D9C56C6E55B5` (`nom`);
+
+--
+-- Index pour la table `nutrition`
+--
+ALTER TABLE `nutrition`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `programme`
+--
+ALTER TABLE `programme`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_3DDCB9FFB5D724CD` (`nutrition_id`),
+  ADD KEY `IDX_3DDCB9FF220C6AD0` (`jour_id`),
+  ADD KEY `IDX_3DDCB9FF7ECF78B0` (`cours_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `cours`
+--
+ALTER TABLE `cours`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `fos_user`
+--
+ALTER TABLE `fos_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `jour`
+--
+ALTER TABLE `jour`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT pour la table `nutrition`
+--
+ALTER TABLE `nutrition`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `programme`
+--
+ALTER TABLE `programme`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `programme`
+--
+ALTER TABLE `programme`
+  ADD CONSTRAINT `FK_3DDCB9FF220C6AD0` FOREIGN KEY (`jour_id`) REFERENCES `jour` (`id`),
+  ADD CONSTRAINT `FK_3DDCB9FF7ECF78B0` FOREIGN KEY (`cours_id`) REFERENCES `cours` (`id`),
+  ADD CONSTRAINT `FK_3DDCB9FFB5D724CD` FOREIGN KEY (`nutrition_id`) REFERENCES `nutrition` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
